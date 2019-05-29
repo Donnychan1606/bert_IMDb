2a9a3cfea1791550f2a4205b9426dc7fd955e824
Donnychan1606 <36060779+Donnychan1606@users.noreply.github.com>
2019-05-29 11:17:22 +0800

    LSTM model

---
+++ lstm_emotionAnalysis.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+from keras.layers.core import Activation, Dense
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential
+from keras.preprocessing import sequence
+from sklearn.model_selection import train_test_split
+import collections
+import nltk
+nltk.download('stopwords')
+import nltk
+nltk.download('punkt')
+import numpy as np
+
+## EDA 
+maxlen = 0
+word_freqs = collections.Counter()
+num_recs = 0
+with open('train.csv','r') as f:
+    for line in f:
+        line=line.rstrip()
+        label=line[-1]
+        #print(label)
+        sentence=line[1:len(line)-3]
+        if label!='0' and label!='1': 
+            continue
+        #print(sentence)
+        #label, sentence = line.strip().split("\t")
+        words = nltk.word_tokenize(sentence.lower())
+        if len(words) > maxlen:
+            maxlen = len(words)
+        for word in words:
+            word_freqs[word] += 1
+        num_recs += 1
+print('max_len ',maxlen)
+print('nb_words ', len(word_freqs))
+
+## 准备数据
+MAX_FEATURES = 2000
+MAX_SENTENCE_LENGTH = 40
+vocab_size = min(MAX_FEATURES, len(word_freqs)) + 2
+word2index = {x[0]: i+2 for i, x in enumerate(word_freqs.most_common(MAX_FEATURES))}
+word2index["PAD"] = 0
+word2index["UNK"] = 1
+index2word = {v:k for k, v in word2index.items()}
+X = np.empty(num_recs,dtype=list)
+y = np.zeros(num_recs)
+i=0
+with open('train.csv','r') as f:
+    for line in f:
+        line=line.rstrip()
+        label=line[-1]
+        sentence=line[1:len(line)-3]
+        if label!='0' and label!='1': 
+            continue
+        words = nltk.word_tokenize(sentence.lower())
+        seqs = []
+        for word in words:
+            if word in word2index:
+                seqs.append(word2index[word])
+            else:
+                seqs.append(word2index["UNK"])
+        X[i] = seqs
+        y[i] = int(label)
+        i += 1
+X = sequence.pad_sequences(X, maxlen=MAX_SENTENCE_LENGTH)
+## 数据划分
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.2, random_state=42)
+## 网络构建
+EMBEDDING_SIZE = 128
+HIDDEN_LAYER_SIZE = 64
+BATCH_SIZE = 32
+NUM_EPOCHS = 10
+model = Sequential()
+model.add(Embedding(vocab_size, EMBEDDING_SIZE,input_length=MAX_SENTENCE_LENGTH))
+model.add(LSTM(HIDDEN_LAYER_SIZE, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1))
+model.add(Activation("sigmoid"))
+model.compile(loss="binary_crossentropy", optimizer="adam",metrics=["accuracy"])
+## 网络训练
+model.fit(Xtrain, ytrain, batch_size=BATCH_SIZE, epochs=NUM_EPOCHS,validation_data=(Xtest, ytest))
+## 预测
+score, acc = model.evaluate(Xtest, ytest, batch_size=BATCH_SIZE)
+print("\nTest score: %.3f, accuracy: %.3f" % (score, acc))
+print('{}   {}      {}'.format('预测','真实','句子'))
+for i in range(5):
+    idx = np.random.randint(len(Xtest))
+    xtest = Xtest[idx].reshape(1,40)
+    ylabel = ytest[idx]
+    ypred = model.predict(xtest)[0][0]
+    sent = " ".join([index2word[x] for x in xtest[0] if x != 0])
+    print(' {}      {}     {}'.format(int(round(ypred)), int(ylabel), sent))
